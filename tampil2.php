911e98b11735ab440524e9cd2520ae3ea48365dc
LintangRamadhanuS <dhanusetyawan10@gmail.com>
2021-10-14 08:34:42 +0700

    Commit 1

---
+++ tampil2.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+      <header>
+    <div class="container">
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="getbootstrap.html">Home</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>   
+          </nav>
+        </header>
+        <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+              <h1 class="display-5 fw-bold">Selamat Datang</h1>
+              <p class="col-md-8 fs-4">
+
+              <?php
+
+// menangkap data nama dengan method post
+$nama = $_POST['nama'];
+$jenis = $_POST['jenis'];
+$agama = $_POST['agama'];
+$alamat = $_POST['Alamat'];
+$TL = $_POST['TL'];
+$tanggal = $_POST['tanggal'];
+$hobi = $_POST['hobi'];
+$cita = $_POST['cita'];
+$usia = $_POST['usia'];
+$sekolah = $_POST['sekolah'];
+
+
+echo "Nama kalian adalah : " . $nama;
+echo "<br/>";
+echo "Jenis Kelamin kalian adalah : " . $jenis;
+echo "<br/>";
+echo "Agama kalian adalah : " . $agama;
+echo "<br/>";
+echo "Alamat kalian adalah : " . $alamat;
+echo "<br/>";
+echo "Tempat Lahir  kalian adalah : " . $TL;
+echo "<br/>";
+echo "Tanggal Lahir kalian adalah : " . $tanggal;
+echo "<br/>";
+echo "Hobi kalian adalah : " . $hobi;
+echo "<br/>";
+echo "Cita-cita kalian adalah  : " . $cita;
+echo "<br/>";
+echo "Usia kalian adalah : " . $usia;
+echo "<br/>";
+echo "Asal Sekolah kalian adalah : " . $sekolah;
+echo "<br/>";
+?>
+
+    </p>
+            </div>
+            <footer class="pt-3 mt-4 text-muted border-top">
+                &copy; 2021
+              </footer>
+          </div>
+
+          
+        
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
